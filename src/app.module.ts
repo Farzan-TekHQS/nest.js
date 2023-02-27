2808d882834a026f178dd1a5eaa5117a300da104
Farzan-TekHQS <farzan.hassan@tekhqs.com>
2023-02-27 12:17:12 +0500

    Connecting mongoose ......

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './modules/quiz/quiz.module';
 
 @Module({
-  imports: [QuizModule],
+  imports: [QuizModule, MongooseModule.forRoot('mongodb+srv://Farzan:<Farzan>@cluster0.g01eb7r.mongodb.net/test')],
   controllers: [AppController],
   providers: [AppService],
 })
